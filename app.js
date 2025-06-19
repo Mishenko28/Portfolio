d5e09e6a9b16ba0bd65c5014b54d2261356b2b60
John Thomas <johnthomasalog@gmail.com>
2025-06-20 01:54:50 +0800

    Updates

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
     {
         "particles": {
             "number": {
-                "value": 20,
+                "value": 15,
                 "density": {
                     "enable": true,
                     "value_area": 100
